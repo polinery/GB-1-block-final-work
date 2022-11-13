3fab69c00efb5e845292db8288c632bb251cde27
polinery <polinery@bk.ru>
2022-11-14 00:30:03 +0300

    Добавлена реализация программы на C#

---
+++ Программа/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+string[] startArray = {"hello", "2", "world", ":-)"};
+// ["hello", "2", "world", ":-)"]
+// ["1234", "1567", "-2", "computer science"]
+// ["Russia", "Denmark", "Kazan"]
+
+var newArray = startArray.Where(x => x.Length < 4).ToArray();
+
+WriteLine($"[{String.Join(", ", newArray)}]");
